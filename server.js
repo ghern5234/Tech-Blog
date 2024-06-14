c1480d2833b4da356c8d0c23fd1375b960f98ca3
George <ghern5234@gmail.com>
2024-06-14 01:22:56 -0500

    fixed and intialized server. added schema, index contorller,  and connction config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express'); // Import express
 const path = require('path'); // Import path to work with files
 const session = require('express-session'); // Import express session for managing sessions and storing sessions in cookies
 const exphbs = require('express-handlebars'); // Import express handlebars to use Handlebars as templating engine
-// const routes = require('./controllers'); //Import routes from controllers directory
+const routes = require('./controllers'); //Import routes from controllers directory
 const dotenv =  require("dotenv");
 const sequelize = require('./config/connection'); // Import the Sequelize instance for database connection configuration
 const SequelizeStore = require("connect-session-sequelize")(session.Store); //Import the Sequelize store for session storage
@@ -47,10 +47,10 @@ app.set("view engine", 'handlebars');
 
 app.use(express.json()); // Middleware to parse the JSON data in the request body
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded data in the request body. extended: true allows for rich obj and arrays to be encoded into the URL-encoded format
-app.use(express.static(path.join__dirname, "public")); // Middleware to serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public"))); // Middleware to serve static files from the 'public' directory
 app.use(routes); // Set the routes obj for various endpoints in the applicaiton
 
-sequelize.syn({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () =>
     console.log(`Server listening on: http://localhost:${PORT}`)
 );
